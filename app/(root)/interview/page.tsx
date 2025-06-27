fa332127ffc520efa2b0d80656f35602726c2fb7
aksh10patil <rajdeeppatil2021@gmail.com>
2025-06-27 17:31:32 +0530

    Added the interview feature

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,21 @@
+
+
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+
+        <h3>
+            Interview Generations
+        </h3>
+
+        <Agent userName="You" userId="user1"/>
+    
+    
+    </>
+  )
+}
+
+export default page
